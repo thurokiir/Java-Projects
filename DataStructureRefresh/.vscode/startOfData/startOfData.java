4426ba26180fcdefe3b9e8a358be9bae218aa22c
Sebastian Andersson <sanderss@eecs.wsu.edu>
2023-03-21 16:06:02 -0700

    File Sorting complete

--- DataStructureRefresh/.vscode/startOfData/startOfData.java
+++ DataStructureRefresh/.vscode/startOfData/startOfData.java
@@ -12,14 +12,18 @@ public class startOfData{
         String[][] baseballResults = testOne.csvFinder();
 
         for(int i = 0; i < baseballResults.length; i++){
-            System.out.println("Round " + i%50);
+
             System.out.println();
+            System.out.println("Round " + i%50);
 
             for(int j = 0; j < 15; j++){
                 System.out.print(baseballResults[i][j]+ "  ,");
             }
             System.out.println();
         }
+
+    
+    
 
     }
 }
--- DataStructureRefresh/.vscode/startOfData/testClass.java
+++ DataStructureRefresh/.vscode/startOfData/testClass.java
@@ -136,6 +136,16 @@ public class testClass {
 
         File[] files = directory.listFiles((dir,name) -> name.endsWith(".csv"));
 
+        Arrays.sort(files, new Comparator<File>(){
+            public int compare(File f1, File f2){
+                return f1.getName().compareTo(f2.getName());
+            }
+        });
+
+        for(File file: files){
+            System.out.println(file.getName());
+        }
+
         int row = 0;
         int col = 0;
 
