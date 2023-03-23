ca1125077f6fcfba3d701ef8ac002ba417b5dd0b
Sebastian Andersson <sanderss@eecs.wsu.edu>
2023-03-22 21:23:59 -0700

    generic2

--- DataStructureRefresh/.vscode/startOfData/DataStorageNode.java
+++ DataStructureRefresh/.vscode/startOfData/DataStorageNode.java
@@ -6,12 +6,17 @@ public class DataStorageNode<T> {
     private T name;
     private int score;
     
-    public DataStorageNode(T name, int score){
-        
-
+    public DataStorageNode(){
         DataStorageNode<T> front = null;
         DataStorageNode<T> back = null;
-        //We'll test if we have to instantiate these later
+        this.name = null;
+        this.score = 0; //no player should have a score of zero, use zero as conditional to determine if the node should be initialized.
+    }
+
+    public DataStorageNode(T name, int score){
+        
+        DataStorageNode<T> front = null;
+        DataStorageNode<T> back = null;
         this.name = name;
         this.score = score;
     }
